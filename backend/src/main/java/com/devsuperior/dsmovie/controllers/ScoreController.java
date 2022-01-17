68baf5eadccf7f89e342a16682ab9f8d13efb1b6
Thiago dos Santos <thisanti@hotmail.com>
2022-01-16 23:28:32 -0300

    Trying one more time

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -2,8 +2,8 @@ package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.dto.ScoreDTO;
-import com.devsuperior.dsmovie.services.MovieService;
 import com.devsuperior.dsmovie.services.ScoreService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +18,8 @@ public class ScoreController {
     private ScoreService scoreService;
 
     @PutMapping
-    public MovieDTO saveScore(@RequestBody ScoreDTO scoreDTO){
-        var movieDTO = scoreService.saveScore(scoreDTO);
+    public MovieDTO saveScore(@RequestBody ScoreDTO scoreDTO) {
+        MovieDTO movieDTO = scoreService.saveScore(scoreDTO);
         return movieDTO;
     }
 }
